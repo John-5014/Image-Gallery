6855d5dc73c732a06826e476cee1139ab725f2db
John-5014 <kaiserjohn52@gmail.com>
2019-11-08 14:30:39 +0300

    adds view file to for index page and display for images

--- images/views.py
+++ images/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+import datetime as dt 
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 
 def index(request):
-  return HttpResponse('Welcome to the best Image Viewe of all times')
+  return render(request,'index.html')
+
+def images_of_day(request):
+  date =dt.date.today()
+  return render(request,'all-images/today-images.html', {"date":date,})
+
+
+
+  
