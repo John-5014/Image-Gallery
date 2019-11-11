12229aea6ecfc77ce4ccec7ee6ed8055a0397108
John-5014 <kaiserjohn52@gmail.com>
2019-11-11 04:10:40 +0300

    add search view functionality with render

--- images/views.py
+++ images/views.py
@@ -1,15 +1,41 @@
 import datetime as dt 
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
+from .models import Image
 
 # Create your views here.
 
 def index(request):
-  return render(request,'index.html')
 
-def images_of_day(request):
-  date =dt.date.today()
-  return render(request,'all-images/today-images.html', {"date":date,})
+  image = Image.objects.all()
+
+
+
+
+  
+  return render(request,'all-images/index.html',{"image":image})
+
+
+def about(request):
+
+  return render(request,'all-images/about.html')
+
+def search_results(request):
+  if 'image' in request.GET and request.GET["image"]:
+    category = request.GET.get("image")
+    searched_images = Image.search_by_category(category)
+
+    message = f"{category}"
+    return render(request, 'all-images/search.html',{"message":message, "images":searched_images})
+
+  else:
+    message = "You haven't searched for any term"
+    return render(request,'all-images/search.html',{"message":message})
+
+
+
+
+
 
 
 
